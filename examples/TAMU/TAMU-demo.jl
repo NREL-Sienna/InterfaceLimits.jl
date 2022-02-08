c1c5b2d0caa7202bb368d18d6419edb50ce151ba
cbarrows <clayton.barrows@nrel.gov>
2022-02-07 21:49:08 -0700

    updating tamu exmaple

--- examples/TAMU/TAMU-demo.jl
+++ examples/TAMU/TAMU-demo.jl
@@ -2,6 +2,7 @@
 
 using PowerSystems
 using InterfaceLimits
+using Xpress #Use a good solver for this
 
 #Download the data from https://electricgrids.engr.tamu.edu/
 
@@ -13,4 +14,11 @@ sys = System(
 set_units_base_system!(sys, "natural_units") # set units to MW
 
 # calculate interfafce limits
-interface_lims = find_interface_limits(sys)
+v = 160.0 #set voltage threshold
+# make a branch filter function to select available branches with endpoints above the v theshold
+bf =
+    x ->
+        get_available(x) &&
+            get_base_voltage(get_from(get_arc(x))) >= v &&
+            get_base_voltage(get_to(get_arc(x))) >= v
+interface_lims = find_interface_limits(sys, Xpress.Optimizer, bf)
