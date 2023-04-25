bbe014bf5fbfa930042dbe068ad2c76337a43d8f
Clayton Barrows <cbarrows@el1.ib0.cm.hpc.nrel.gov>
2023-04-25 09:18:58 -0600

    making single limit calc

--- examples/rts/rts-interface-demo.jl
+++ examples/rts/rts-interface-demo.jl
@@ -1,6 +1,8 @@
 using InterfaceLimits
 using PowerSystems
+using PowerNetworkMatrices
 using HiGHS
+solver = HiGHS.Optimizer
 # use a simple rts system
 sys = System(
     joinpath(dirname(dirname(pathof(InterfaceLimits))), "examples", "rts", "RTS-GMLC.RAW"),
@@ -11,13 +13,22 @@ set_units_base_system!(sys, "natural_units")
 
 # calculate interfafce limits
 @info "calculating n-0 interface limits"
-@time interface_lims = find_interface_limits(sys, HiGHS.Optimizer);
+@time interface_lims = find_interface_limits(sys, solver);
 
 @info "calculating n-0 interface limits with single problem"
-@time interface_lims = find_monolithic_interface_limits(sys, HiGHS.Optimizer);
+@time interface_lims = find_monolithic_interface_limits(sys, solver);
 
+lodf = LODF(sys)
 @info "calculating n-1 interface limits"
-interface_lims = find_interface_limits(sys, HiGHS.Optimizer, security = true)
+interface_lims = find_interface_limits(sys, solver, lodf = lodf, security = true)
 
 @info "calculating n-1 interface limits with single problem"
-interface_lims = find_monolithic_interface_limits(sys, HiGHS.Optimizer, security = true)
+interface_lims = find_monolithic_interface_limits(sys, solver, lodf = lodf, security = true)
+
+
+@info "calculating n-0 interface limits for just 1 interface"
+interfaces = find_interfaces(sys)
+interface_key = first(collect(keys(interfaces)))
+interface = interfaces[interface_key]
+@time interface_lims = find_interface_limits(sys, solver, interface_key, interface, interfaces);
+@time interface_lims = find_interface_limits(sys, solver, interface_key, interface, interfaces, lodf = lodf, security = true);
--- src/InterfaceLimits.jl
+++ src/InterfaceLimits.jl
@@ -32,18 +32,27 @@ end
 
 function find_neighbor_interfaces(
     interfaces::Dict{Pair{String,String},Vector{ACBranch}},
-    hops = 1,
+    interface_key::Pair{String,String},
+    hops::Int64 = 1,
 )
     neighbors = Dict{Pair{String,String},Set{String}}()
     all_interfaces = collect(keys(interfaces))
-    for interface in all_interfaces
-        community = Set(interface)
-        for hop = 1:hops
-            add_neighbors =
-                findall(x -> (first(x) ∈ community || last(x) ∈ community), all_interfaces)
-            union!(community, union(Set.(all_interfaces[add_neighbors])...))
-        end
-        neighbors[interface] = community
+    community = Set(interface_key)
+    for hop = 1:hops
+        add_neighbors =
+            findall(x -> (first(x) ∈ community || last(x) ∈ community), all_interfaces)
+        union!(community, union(Set.(all_interfaces[add_neighbors])...))
+    end
+    return community
+end
+
+function find_neighbor_interfaces(
+    interfaces::Dict{Pair{String,String},Vector{ACBranch}},
+    hops::Int64 = 1,
+)
+    neighbors = Dict{Pair{String,String},Set{String}}()
+    for interface in collect(keys(interfaces))
+        neighbors[interface] = find_neighbor_interfaces(interfaces, interface, hops)
     end
     return neighbors
 end
@@ -52,6 +61,137 @@ end
 function clean_sys!(sys)
     remove_components!(!get_available, sys, Device)
     remove_components!(!get_available, sys, HVDCLine)
+end
+
+function add_variables!(m, inames, in_branches, injection_buses, security)
+    # create flow variables for branches
+    @variable(m, F[inames, get_name.(in_branches)])
+    @variable(m, I[inames])
+    @variable(m, P[inames, get_name.(injection_buses)])
+    vars = Dict("flow" => F, "interface" => I, "injection" => P)
+    if security
+        @variable(m, CF[inames, get_name.(in_branches), get_name.(in_branches)])
+        vars["cont_flow"] = CF
+    end
+
+    return vars
+end
+
+function add_constraints!(m, vars, interface_key, interface, injection_buses, gen_buses, load_buses, in_branches, ptdf, ptdf_threshold, security, lodf)
+    F = vars["flow"]
+    I = vars["interface"]
+    P = vars["injection"]
+    if security
+        CF = vars["cont_flow"]
+    end
+
+    for ikey in [interface_key, reverse(interface_key)]
+        forward = ikey == interface_key ? 1 : -1
+        iname = join(ikey, "_")
+        for b in injection_buses
+            if b ∈ setdiff(gen_buses, load_buses) # only gens connected
+                @constraint(m, P[iname, get_name(b)] >= 0.0)
+            elseif b ∈ setdiff(load_buses, gen_buses) # only loads connected
+                @constraint(m, P[iname, get_name(b)] <= 0.0)
+            end
+        end
+
+        for br in in_branches
+            name = get_name(br)
+            @constraint(m, get_rate(br) >= F[iname, name] >= get_rate(br) * -1)
+
+            ptdf_expr = [
+                ptdf[name, get_number(b)] * P[iname, get_name(b)] for
+                b in injection_buses if
+                abs(ptdf[name, get_number(b)]) > ptdf_threshold
+            ]
+            push!(ptdf_expr, 0.0)
+            @constraint(
+                m,
+                F[iname, name] ==sum(ptdf_expr) * forward
+            )
+            # OutageFlowX = PreOutageFlowX + LODFx,y* PreOutageFlowY
+            if security
+                isnothing(lodf) && error("lodf must be defined")
+                for cbr in in_branches
+                    cname = get_name(cbr)
+                    @constraint(
+                        m,
+                        get_rate(br) >= CF[iname, name, cname] >= get_rate(br) * -1
+                    )
+                    @constraint(
+                        m,
+                        CF[iname, name, cname] ==
+                        F[iname, name] + lodf[name, cname] * F[iname, cname]
+                    )
+                end
+            end
+        end
+
+        @constraint(m, I[iname] == sum(F[iname, get_name(br)] for br in interface))
+    end
+end
+
+function find_interface_limits(
+    sys,
+    solver,
+    interface_key,
+    interface,
+    interfaces;
+    branch_filter = x -> get_available(x),
+    ptdf = VirtualPTDF(sys),
+    lodf = nothing,
+    security = false, # n-1 security
+    hops = 1, # neighboring areas to include
+    ptdf_threshold = 1e-6, #rounding threshold for including ptdf
+)
+    interface_neighbors = find_neighbor_interfaces(interfaces, hops)
+    neighbors = interface_neighbors[interface_key]
+    branches = get_components(branch_filter, ACBranch, sys) # could filter for monitored lines here
+    in_branches = filter(
+        x -> (
+            get_name(get_area(get_from(get_arc(x)))) ∈ neighbors ||
+            get_name(get_area(get_to(get_arc(x)))) ∈ neighbors
+        ),
+        collect(branches),
+    )
+
+    #inames = join.(vcat(collect(keys(interfaces)), reverse.(keys(interfaces))), "_")
+    inames = join.(vcat(interface_key, reverse(interface_key)), "_")
+    gen_buses = filter(
+        x -> get_name(get_area(x)) ∈ neighbors,
+        Set(get_bus.(get_components(get_available, Generator, sys))),
+    )
+    load_buses = filter(
+        x -> get_name(get_area(x)) ∈ neighbors,
+        Set(get_bus.(get_components(get_available, ElectricLoad, sys))),
+    )
+    injection_buses = union(gen_buses, load_buses)
+
+    # Build a JuMP Model
+    m = Model(solver)
+    vars = add_variables!(m, inames, in_branches, injection_buses, security)
+    add_constraints!(m, vars, interface_key, interface, injection_buses, gen_buses, load_buses, in_branches, ptdf, ptdf_threshold, security, lodf)
+    # make max objective
+    @objective(m, Max, sum(vars["interface"]))
+
+    # solve the problem
+    @info "Solving interface limit problem with" solver
+    optimize!(m)
+
+    # return the interface values
+    interface_lims = value.(vars["interface"])
+    df = DataFrame(
+        :interface => interface_lims.axes[1],
+        :transfer_limit => interface_lims.data,
+    )
+
+    interface_cap = DataFrame(
+        :interface => inames,
+        :sum_capacity => ones(2) .* sum(get_rate.(interface)),
+    )
+    @show df = leftjoin(df, interface_cap, on = :interface)
+    return df
 end
 
 function find_interface_limits(
@@ -66,114 +206,17 @@ function find_interface_limits(
 )
 
     interfaces = find_interfaces(sys, branch_filter)
-    interface_neighbors = find_neighbor_interfaces(interfaces, hops)
-    branches = get_components(branch_filter, ACBranch, sys) # could filter for monitored lines here
-
     results_dfs = []
 
     ik = 0
     for (interface_key, interface) in interfaces
         ik+=1
-        neighbors = interface_neighbors[interface_key]
-        in_branches = filter(
-            x -> (
-                get_name(get_area(get_from(get_arc(x)))) ∈ neighbors ||
-                get_name(get_area(get_to(get_arc(x)))) ∈ neighbors
-            ),
-            collect(branches),
-        )
-
-        #inames = join.(vcat(collect(keys(interfaces)), reverse.(keys(interfaces))), "_")
-        inames = join.(vcat(interface_key, reverse(interface_key)), "_")
-        # Build a JuMP Model
         @info " $ik/$(length(interfaces)) Building interface limit optimization model " interface_key
-        m = Model(solver)
-        # create flow variables for branches
-        @variable(m, F[inames, get_name.(in_branches)])
-        security && @variable(m, CF[inames, get_name.(in_branches), get_name.(in_branches)])
-        # create variables and interfaces
-        @variable(m, I[inames])
-
-        gen_buses = filter(
-            x -> get_name(get_area(x)) ∈ neighbors,
-            Set(get_bus.(get_components(get_available, Generator, sys))),
-        )
-        load_buses = filter(
-            x -> get_name(get_area(x)) ∈ neighbors,
-            Set(get_bus.(get_components(get_available, ElectricLoad, sys))),
-        )
-        injection_buses = union(gen_buses, load_buses)
-
-        @variable(m, P[inames, get_name.(injection_buses)])
-
-        for ikey in [interface_key, reverse(interface_key)]
-            forward = ikey == interface_key ? 1 : -1
-            iname = join(ikey, "_")
-            for b in injection_buses
-                if b ∈ setdiff(gen_buses, load_buses) # only gens connected
-                    @constraint(m, P[iname, get_name(b)] >= 0.0)
-                elseif b ∈ setdiff(load_buses, gen_buses) # only loads connected
-                    @constraint(m, P[iname, get_name(b)] <= 0.0)
-                end
-            end
-
-            for br in in_branches
-                name = get_name(br)
-                @constraint(m, get_rate(br) >= F[iname, name] >= get_rate(br) * -1)
-
-                ptdf_expr = [
-                    ptdf[name, get_number(b)] * P[iname, get_name(b)] for
-                    b in injection_buses if
-                    abs(ptdf[name, get_number(b)]) > ptdf_threshold
-                ]
-                push!(ptdf_expr, 0.0)
-                @constraint(
-                    m,
-                    F[iname, name] ==sum(ptdf_expr) * forward
-                )
-                # OutageFlowX = PreOutageFlowX + LODFx,y* PreOutageFlowY
-                if security
-                    for cbr in in_branches
-                        cname = get_name(cbr)
-                        @constraint(
-                            m,
-                            get_rate(br) >= CF[iname, name, cname] >= get_rate(br) * -1
-                        )
-                        @constraint(
-                            m,
-                            CF[iname, name, cname] ==
-                            F[iname, name] + lodf[name, cname] * F[iname, cname]
-                        )
-                    end
-                end
-            end
-
-            @constraint(m, I[iname] == sum(F[iname, get_name(br)] for br in interface))
-        end
-        # make max objective
-        @objective(m, Max, sum(I))
-
-        # solve the problem
-        @info "Solving interface limit problem with" solver
-        optimize!(m)
-
-        # return the interface values
-        interface_lims = value.(I)
-        df = DataFrame(
-            :interface => interface_lims.axes[1],
-            :transfer_limit => interface_lims.data,
-        )
-
+        df = find_interface_limits(sys, solver, interface_key, interface, interfaces, branch_filter = branch_filter, ptdf = ptdf, lodf = lodf, security = security, hops = hops, ptdf_threshold = ptdf_threshold)
         push!(results_dfs, df)
     end
 
-    # add capacities to df
-    interface_cap = DataFrame(
-        :interface =>
-            join.(vcat(collect(keys(interfaces)), reverse.(keys(interfaces))), "_"),
-        :sum_capacity => repeat(sum.([get_rate.(br) for br in values(interfaces)]), 2),
-    )
-    df = leftjoin(vcat(results_dfs...), interface_cap, on = :interface)
+    df = vcat(results_dfs...)
 
     @info "Interface limits calculated" df
     return df
@@ -184,7 +227,7 @@ function find_monolithic_interface_limits(
     solver;
     branch_filter = x -> get_available(x),
     ptdf = VirtualPTDF(sys),
-    lodf = LODF(sys),
+    lodf = nothing,
     security = false,
     ptdf_threshold = 1e-6, #rounding threshold for including ptdf
 )
@@ -193,77 +236,26 @@ function find_monolithic_interface_limits(
     m = Model(solver)
 
     interfaces = find_interfaces(sys, branch_filter)
-    branches = get_components(branch_filter, ACBranch, sys) # could filter for monitored lines here
-
-    inames = join.(vcat(collect(keys(interfaces)), reverse.(keys(interfaces))), "_")
-    # create flow variables for branches
-    @variable(m, F[inames, get_name.(branches)])
-    security && @variable(m, CF[inames, get_name.(branches), get_name.(branches)])
-    # create variables and interfaces
-    @variable(m, I[inames])
-
+    in_branches = get_components(branch_filter, ACBranch, sys) # could filter for monitored lines here
     gen_buses = Set(get_bus.(get_components(get_available, Generator, sys)))
     load_buses = Set(get_bus.(get_components(get_available, ElectricLoad, sys)))
     injection_buses = union(gen_buses, load_buses)
 
-    @variable(m, P[inames, get_name.(injection_buses)])
-
+    inames = join.(vcat(collect(keys(interfaces)), reverse.(keys(interfaces))), "_")
+    vars = add_variables!(m, inames, in_branches, injection_buses, security)
     for (interface_key, interface) in interfaces
-        for ikey in [interface_key, reverse(interface_key)]
-            forward = ikey == interface_key ? 1 : -1
-            iname = join(ikey, "_")
-            for b in injection_buses
-                if b ∈ setdiff(gen_buses, load_buses) # only gens connected
-                    @constraint(m, P[iname, get_name(b)] >= 0.0)
-                elseif b ∈ setdiff(load_buses, gen_buses) # only loads connected
-                    @constraint(m, P[iname, get_name(b)] <= 0.0)
-                end
-            end
-
-            for br in branches
-                name = get_name(br)
-                @constraint(m, get_rate(br) >= F[iname, name] >= get_rate(br) * -1)
-
-                @constraint(
-                    m,
-                    F[iname, name] ==
-                    sum([
-                        ptdf[name, get_number(b)] * P[iname, get_name(b)] for
-                        b in injection_buses if
-                        abs(ptdf[name, get_number(b)]) > ptdf_threshold
-                    ]) * forward
-                )
-
-                # OutageFlowX = PreOutageFlowX + LODFx,y* PreOutageFlowY
-                if security
-                    for cbr in branches
-                        cname = get_name(cbr)
-                        @constraint(
-                            m,
-                            get_rate(br) >= CF[iname, name, cname] >= get_rate(br) * -1
-                        )
-                        @constraint(
-                            m,
-                            CF[iname, name, cname] ==
-                            F[iname, name] + lodf[name, cname] * F[iname, cname]
-                        )
-                    end
-                end
-            end
-
-            @constraint(m, I[iname] == sum(F[iname, get_name(br)] for br in interface))
-        end
+        add_constraints!(m, vars, interface_key, interface, injection_buses, gen_buses, load_buses, in_branches, ptdf, ptdf_threshold, security, lodf)
     end
 
     # make max objective
-    @objective(m, Max, sum(I))
+    @objective(m, Max, sum(vars["interface"]))
 
     # solve the problem
     @info "Solving interface limit problem with" solver
     optimize!(m)
 
     # return the interface values
-    interface_lims = value.(I)
+    interface_lims = value.(vars["interface"])
     df = DataFrame(
         :interface => interface_lims.axes[1],
         :transfer_limit => interface_lims.data,
